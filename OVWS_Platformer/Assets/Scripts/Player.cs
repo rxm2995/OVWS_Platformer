522d6c32dd2e228150b8a67d8404dc22ed2556fa
sas8463 <sas8463@GDD-19.main.ad.rit.edu>
2015-09-27 17:52:02 -0400

    Flawed Movement

--- OVWS_Platformer/Assets/Scripts/Player.cs
+++ OVWS_Platformer/Assets/Scripts/Player.cs
@@ -10,6 +10,21 @@ public class Player : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if(Input.GetKey("w"))
+		{
+
+		}
+		else if(Input.GetKey("s"))
+		{
+			
+		}
+		if(Input.GetKey("a"))
+		{
+			transform.position = new Vector3(transform.position.x - 0.25f, transform.position.y, transform.position.z);
+		}
+		else if(Input.GetKey("d"))
+		{
+			transform.position = new Vector3(transform.position.x + 0.25f, transform.position.y, transform.position.z);
+		}
 	}
 }
