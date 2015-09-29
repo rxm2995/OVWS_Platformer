b46859d4e72415630a812872a12a931cb52067e9
Josh Ladisic <jal5313@gmail.com>
2015-09-29 16:26:50 -0400

    Attempted to implement Movement
    Movement still horribly broken. I'm quite unsure of why or how to fix
    it.
    
    Changed inputs to a switch.

--- OVWS_Platformer/Assets/Scripts/Player.cs
+++ OVWS_Platformer/Assets/Scripts/Player.cs
@@ -3,28 +3,34 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
+	public float maxSpeed = 6f;
+	public float maxForce = 3f;
+	public float mass = 1f;
+	public float gravity = 20f;
+
+	protected Vector3 velocity;
+	protected Vector3 acceleration;
+
+	protected CharacterController charControl;
+
 	// Use this for initialization
 	void Start () {
-	
+		charControl = gameObject.GetComponent<CharacterController> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetKey("w"))
-		{
 
+		string input;
+		input = Input.inputString;
+		switch (input) {
+		case "a":
+			charControl.Move (new Vector3 (0, transform.position.y, 0) * 10 * Time.deltaTime);
+			break;
+		case "d":
+			charControl.Move (new Vector3 (0, transform.position.y, 0) * -10 * Time.deltaTime);
+			break;
 		}
-		else if(Input.GetKey("s"))
-		{
-			
-		}
-		if(Input.GetKey("a"))
-		{
-			transform.position = new Vector3(transform.position.x - 0.25f, transform.position.y, transform.position.z);
-		}
-		else if(Input.GetKey("d"))
-		{
-			transform.position = new Vector3(transform.position.x + 0.25f, transform.position.y, transform.position.z);
-		}
+		
 	}
-}
+} 
