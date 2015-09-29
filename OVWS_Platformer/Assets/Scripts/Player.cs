9521b0f858df9dd8cfaf458dfbfac8d911b6140e
Ryan Mignini <rxm2995@rit.edu>
2015-09-29 18:52:21 -0400

    Fixed Movement
    There aren't any character-specific abilities and the jumping is pretty
    ugly, but it works without breaking anything.
    WASD and arrow keys are both supported, with Up, W, and Space all
    available for jumping.

--- OVWS_Platformer/Assets/Scripts/Player.cs
+++ OVWS_Platformer/Assets/Scripts/Player.cs
@@ -6,31 +6,52 @@ public class Player : MonoBehaviour {
 	public float maxSpeed = 6f;
 	public float maxForce = 3f;
 	public float mass = 1f;
-	public float gravity = 20f;
 
+	public float jumpForce = 3f;
+	public float jumpDecayRate = 0.8f;
+	public float minYVelocity = 0.01f;
+	
+	protected bool onGround;
 	protected Vector3 velocity;
 	protected Vector3 acceleration;
-
 	protected CharacterController charControl;
 
+	private float yVelocity;
+
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 		charControl = gameObject.GetComponent<CharacterController> ();
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
+		Vector3 velocity = Vector3.zero;
 
-		string input;
-		input = Input.inputString;
-		switch (input) {
-		case "a":
-			charControl.Move (new Vector3 (0, transform.position.y, 0) * 10 * Time.deltaTime);
-			break;
-		case "d":
-			charControl.Move (new Vector3 (0, transform.position.y, 0) * -10 * Time.deltaTime);
-			break;
+		//Input handling
+		if (Input.GetKey (KeyCode.A) || Input.GetKey (KeyCode.LeftArrow))
+		{
+			velocity.x += -1 * maxSpeed;// * Time.deltaTime;
 		}
+		if (Input.GetKey (KeyCode.D) || Input.GetKey (KeyCode.RightArrow))
+		{
+			velocity.x += maxSpeed;// * Time.deltaTime;
+		}
+		if (onGround && (Input.GetKeyDown (KeyCode.W) || Input.GetKeyDown (KeyCode.UpArrow) || Input.GetKeyDown (KeyCode.Space)))
+		{
+			yVelocity = jumpForce;
+		}
+
+		//Jump arc handling
+		if (yVelocity >= minYVelocity)
+		{
+			yVelocity *= jumpDecayRate;
+			charControl.Move(new Vector3(0, yVelocity, 0));
+
+		}
+		//Normal movement handling
+		onGround = charControl.SimpleMove (velocity);
 		
 	}
 } 
