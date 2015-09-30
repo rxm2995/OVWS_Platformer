23c62a2eb78b97e6f6c54e9e7563b778780955a3
Ryan Mignini <rxm2995@rit.edu>
2015-09-29 20:14:34 -0400

    Implemented basic position sync
    It's rudimentary, but there. Next priority: Interpolation, to smooth out
    the experience for players receiving transmitted positions.

---
+++ OVWS_Platformer/Assets/Scripts/PlayerSync.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class PlayerSync : NetworkBehaviour
+{
+	public float minDeltaBeforePosSync = 0.1f;
+
+	[SyncVar]
+	private Vector3 syncPos;
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (!isLocalPlayer)
+		{
+			transform.position = syncPos;
+		}
+	}
+
+	void FixedUpdate()
+	{
+		if (isLocalPlayer)
+		{
+			TransmitPosition();
+		}
+	}
+
+	[Client]
+	void TransmitPosition()
+	{
+		if (isLocalPlayer && (transform.position-syncPos).magnitude >= minDeltaBeforePosSync)
+		{
+			CmdSyncPosition(transform.position);
+		}
+	}
+
+	[Command]
+	void CmdSyncPosition(Vector3 posToTransmit)
+	{
+		syncPos = posToTransmit;
+	}
+}
