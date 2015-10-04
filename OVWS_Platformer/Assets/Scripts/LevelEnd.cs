bb8d8afec62667a21dad4dac07a9b1e203d17e70
unknown <nas2375@GDD-13.main.ad.rit.edu>
2015-10-04 18:12:24 -0400

    stuff
    work on scene 1 end level trigger

---
+++ OVWS_Platformer/Assets/Scripts/LevelEnd.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelEnd : MonoBehaviour {
+
+	int count;
+	GameObject[] playerReferences;
+
+	void Start () {
+		count = 0;
+		playerReferences = GameObject.FindGameObjectsWithTag ("Player");
+	}
+
+	void Update () {
+		// activates new scene when all players gather on this object
+		if (count == 1) {
+			Debug.Log("audhfiadusfhsuiof");
+			Application.LoadLevel(1);
+		}
+
+	}
+
+	void OnTriggerEnter(Collider other) {
+		count++;
+	}
+	void OnTriggerExit(Collider other) {
+		count--;
+	}
+}
