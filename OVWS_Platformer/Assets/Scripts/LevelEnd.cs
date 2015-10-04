efc9ea52640ce3448158cf69bf78a279387de9f4
sas8463 <sas8463@GDD-12.main.ad.rit.edu>
2015-10-04 18:59:18 -0400

    Scene switching
    Standalone and webplayer builds, scene switch only works for 1 player

--- OVWS_Platformer/Assets/Scripts/LevelEnd.cs
+++ OVWS_Platformer/Assets/Scripts/LevelEnd.cs
@@ -1,23 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.Networking;
 
-public class LevelEnd : MonoBehaviour {
-
+public class LevelEnd : NetworkBehaviour
+{
+	NetworkManager netManager;
 	int count;
 	GameObject[] playerReferences;
 
 	void Start () {
 		count = 0;
 		playerReferences = GameObject.FindGameObjectsWithTag ("Player");
+		netManager = GameObject.FindGameObjectWithTag("NetManager").GetComponent<NetworkManager>();
 	}
 
 	void Update () {
 		// activates new scene when all players gather on this object
 		if (count == 1) {
 			Debug.Log("audhfiadusfhsuiof");
-			Application.LoadLevel(1);
+			//Application.LoadLevel(1);
+			netManager.ServerChangeScene("testScene2");
 		}
-
 	}
 
 	void OnTriggerEnter(Collider other) {
