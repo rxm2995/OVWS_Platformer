cfa1713a72bd48ac0d2442828cf37d7f81174232
Ryan Mignini <rxm2995@rit.edu>
2015-10-07 09:33:19 -0400

    Added third level
    Template for "P2 throws P1 across gap" puzzle. Can currently be solved
    by P1 alone, will be changed once P2's throwing ability is implemented.

---
+++ OVWS_Platformer/Assets/Scripts/PuzzleManager3.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PuzzleManager3 : MonoBehaviour
+{
+	public GameObject switchOne;
+	public GameObject dynamicPlatform;
+
+	private SwitchBehavior switchOneBehavior;
+	private bool puzzleSolved;
+
+	// Use this for initialization
+	void Start ()
+	{
+		switchOneBehavior = switchOne.GetComponent<SwitchBehavior>();
+		puzzleSolved = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if(!puzzleSolved && switchOneBehavior.activated)
+		{
+			puzzleSolved = true;
+			switchOneBehavior.solvePuzzle();
+			dynamicPlatform.transform.position = new Vector3(dynamicPlatform.transform.position.x,
+			                                                 2,
+			                                                 0);
+		}
+	}
+}
