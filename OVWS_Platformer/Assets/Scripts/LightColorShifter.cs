8aefffab25dbfca31782679c0d771bec11d68738
sas8463 <sas8463@IGMSPARE282027.main.ad.rit.edu>
2015-10-13 14:52:17 -0400

    Color Shifter, that may never be used
    Attach to a light, set some values, it shifts between them

---
+++ OVWS_Platformer/Assets/Scripts/LightColorShifter.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightColorShifter : MonoBehaviour {
+	public Light currentlight;
+	public float r;
+	public float g;
+	public float b;
+	public float rChange;
+	public float gChange;
+	public float bChange;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		currentlight.color = new Color(r,g,b);
+		r += rChange;
+		g += gChange;
+		b += bChange;
+		if(r > 1 || r < 0) rChange *= -1;
+		if(g > 1 || g < 0) gChange *= -1;
+		if(b > 1 || b < 0) bChange *= -1;
+	}
+}
