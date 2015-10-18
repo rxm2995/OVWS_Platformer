9d1f2084c4ea67a642cd64d30c9fbc2984292ce6
sas8463 <sas8463@GDD-29.main.ad.rit.edu>
2015-10-18 18:48:18 -0400

    Wall Jumping for Acrobat Player
    Jumping sequence will now consume your ground jump, your wall jump, and
    then your air jump for reference. Timer in this class is used t enhance
    how the player reacts to the controls

--- OVWS_Platformer/Assets/Scripts/AcrobatPlayer.cs
+++ OVWS_Platformer/Assets/Scripts/AcrobatPlayer.cs
@@ -4,6 +4,9 @@ using System.Collections;
 public class AcrobatPlayer : Player
 {
 	private bool canAirJump = true;
+	private bool canWallJump = true;
+	private bool onWall = false;
+	private float timer;
 
 	public override void Start()
 	{
@@ -20,12 +23,78 @@ public class AcrobatPlayer : Player
 	public override void Update ()
 	{
 		canAirJump = onGround || canAirJump;
-		if (!onGround && canAirJump && (Input.GetKeyDown (KeyCode.W) || Input.GetKeyDown (KeyCode.UpArrow) || Input.GetKeyDown (KeyCode.Space)))
+		if(onGround) canWallJump = true;
+//		Debug.Log (charControl.collisionFlags);
+//		if((charControl.collisionFlags & CollisionFlags.Sides) != 0)
+//		{
+//			Debug.Log("Test");
+//			canWallJump = true;
+//		}
+		//Debug.Log("OnWall: "  + onWall);
+		//Debug.Log("CanWallJump: " + canWallJump);
+		if(!onGround)
 		{
-			yVelocity = jumpForce/jumpDecayRate;
-			canAirJump = false;
+			if (onWall && canWallJump && (Input.GetKeyDown (KeyCode.W) || Input.GetKeyDown (KeyCode.UpArrow) || Input.GetKeyDown (KeyCode.Space)))
+			{
+				Debug.Log("Wall Jumped");
+				yVelocity = jumpForce/jumpDecayRate;
+				canWallJump = false;
+				onWall = false;
+			}
+			else if (canAirJump && (Input.GetKeyDown (KeyCode.W) || Input.GetKeyDown (KeyCode.UpArrow) || Input.GetKeyDown (KeyCode.Space)))
+			{
+				yVelocity = jumpForce/jumpDecayRate;
+				canAirJump = false;
+			}	
 		}
-		
+		timer += Time.deltaTime;
 		base.Update();
 	}
+
+	void OnControllerColliderHit(ControllerColliderHit col)
+	{
+		Vector3 hit = col.normal;
+		float angle = Vector3.Angle (hit, Vector3.up);
+		if(Mathf.Approximately(angle, 90) || Mathf.Approximately(angle, 270)) 
+		{
+			Debug.Log("Wall Collision");
+			onWall = true;
+			timer = 0;
+		}
+		if(timer > .1f)
+		{
+			onWall = false;
+		}
+		//else onWall = false;
+//		if(Mathf.Approximately(angle, 90))
+//		{
+//			Debug.Log ("Left");
+//		}
+//		else if(Mathf.Approximately(angle, 270))
+//		{
+//			Debug.Log ("Right");
+//		}
+	}
+
+//	void OnCollisionEnter(Collision col)
+//	{
+//		Debug.Log("Test");
+//		Vector3 hit = col.contacts[0].normal;
+//		Debug.Log(hit);
+//		float angle = Vector3.Angle (hit, Vector3.up);
+//		if(Mathf.Approximately(angle, 90))
+//		{
+//			Debug.Log ("Left");
+//		}
+//		else if(Mathf.Approximately(angle, 270))
+//		{
+//			Debug.Log ("Right");
+//		}
+//	}
+//
+//	void OnCollisionExit(Collision col)
+//	{
+//		//this.transform.gameObject.transform.gameObject.transform.gameObject.gameObject.gameObject.
+//
+//	}
 }
