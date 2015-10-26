bdfe95f43d6e71f00ef71ac80f6d81d9e91aae0a
sas8463 <sas8463@C2435-30.main.ad.rit.edu>
2015-10-26 14:45:30 -0400

    More changes to minimaps
    minimap and its border now hold a 16/9 aspect ratio and adjust based on
    screen size

---
+++ OVWS_Platformer/Assets/Scripts/MinimapBehavior.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MinimapBehavior : MonoBehaviour {
+
+	public RectTransform panel;
+	private Camera thisCamera;
+	float lastScreenHeight;
+	float lastScreenWidth;
+
+	// Use this for initialization
+	void Start () {
+		thisCamera = GetComponent<Camera>();
+		thisCamera.aspect = (16f/9f);
+		panel.sizeDelta = new Vector2(thisCamera.aspect * Screen.width * .575f, thisCamera.aspect * Screen.height * .575f); 
+		lastScreenWidth = Screen.width;
+		lastScreenHeight = Screen.height;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(lastScreenWidth != Screen.width || lastScreenHeight != Screen.height)
+		{
+			lastScreenWidth = Screen.width;
+			lastScreenHeight = Screen.height;
+			thisCamera.aspect = (16f/9f);
+			panel.sizeDelta = new Vector2(thisCamera.aspect * Screen.width * .575f, thisCamera.aspect * Screen.height * .575f); 
+		}
+	}
+}
