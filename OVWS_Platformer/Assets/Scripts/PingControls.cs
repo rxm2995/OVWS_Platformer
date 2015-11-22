c162e0469f67e3d32a2efef07862b87ba2103a30
unknown <nas2375@GDD-11.main.ad.rit.edu>
2015-11-22 18:38:38 -0500

    ping works locally, need to implement networking fix

--- OVWS_Platformer/Assets/Scripts/PingControls.cs
+++ OVWS_Platformer/Assets/Scripts/PingControls.cs
@@ -5,25 +5,52 @@ using UnityEngine.Networking;
 public class PingControls : NetworkBehaviour {
 
 	public GameObject ping;
+	//public GameObject rayCastNet;
+	public Camera playerCamera;
+
+	private bool cameraDragging;
 	private Vector3 clickPosition;
-	private ArrayList pingLocations;
-	private int currentPingIndex;
 
 	void Start () {
-		pingLocations = new ArrayList();
-		currentPingIndex = 0;
+		if (isLocalPlayer) {
+			Cursor.visible = false;
+		}
 	}
 
 	void Update () {
-
-		//isLocalPlayer LMB
 		if (isLocalPlayer) {
-			if (Input.GetMouseButtonDown (0)) {
-				clickPosition = Input.mousePosition;
+		
+			// Add Ping with RMB
+			if (Input.GetMouseButtonDown (1)) {
+				if (cameraDragging)
+				{
+					clickPosition = Input.mousePosition;
+					Vector3 destination = playerCamera.ScreenPointToRay(clickPosition).GetPoint(10);
 
-				Instantiate(ping, new Vector3(0, 0, 0), new Quaternion(0,0,0,0));
-				Debug.Log("ping added");
+					Instantiate(ping, new Vector3(destination.x, destination.y, -1.5f), ping.transform.rotation);
+				}
 			}
+
+			// Activate Camera Dragging with Tab
+			if (Input.GetKeyDown(KeyCode.Tab))
+			{
+				cameraDragging = true;
+
+				//disable player movement
+
+				//move mouse to middle of screen
+				Cursor.lockState = CursorLockMode.Locked;
+				Cursor.lockState = CursorLockMode.None;
+				Cursor.visible = true;
+			}
+
+			/*
+			// Camera Dragging - move mouse to edges of screen to drag camera
+			if (Input.mousePosition.x)
+			{
+
+			}
+			*/
 		}
 	}
 }
---
+++ OVWS_Platformer/Assets/Scripts/pingTimer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pingTimer : MonoBehaviour {
+
+	private float startTime;
+	private float duration = 5;
+	void Start () {
+		startTime = Time.time;
+	}
+
+	void Update () {
+		if (Time.time > startTime + duration) {
+			Destroy(gameObject);
+		}
+	}
+}
