dd2f61e9bd86d57c3fbb386677c54bc11eacda20
unknown <nas2375@C2435-32.main.ad.rit.edu>
2015-11-30 14:54:12 -0500

    Ping progress - messing with CMD / RPC / networking

--- OVWS_Platformer/Assets/Scripts/PingControls.cs
+++ OVWS_Platformer/Assets/Scripts/PingControls.cs
@@ -29,6 +29,20 @@ public class PingControls : NetworkBehaviour {
 					clickPosition = Input.mousePosition;
 					Vector3 destination = playerCamera.ScreenPointToRay(clickPosition).GetPoint(10);
 
+					if (isServer)
+					{
+						Instantiate(ping, new Vector3(destination.x, destination.y, -1.5f), new Quaternion(0, 0, 0, 0));
+						//RpcSpawnPing(destination);
+						NetworkServer.Spawn (ping);
+					}
+					else if (isClient) {
+						Instantiate(ping, new Vector3(destination.x, destination.y, -1.5f), new Quaternion(0, 0, 0, 0));
+						CmdSpawnPing(destination);
+					}
+
+
+					clientOnlySpawnPing(destination);
+					//RpcSpawnPing(destination);
 					CmdSpawnPing(destination);
 				}
 			}
@@ -36,15 +50,31 @@ public class PingControls : NetworkBehaviour {
 			// Activate Camera Dragging with Tab
 			if (Input.GetKeyDown(KeyCode.Tab))
 			{
-				cameraDragging = true;
-
-				//disable player movement
-
-
-				//move mouse to middle of screen
-				Cursor.lockState = CursorLockMode.Locked;
-				Cursor.lockState = CursorLockMode.None;
-				Cursor.visible = true;
+				if (!cameraDragging)
+				{	
+					cameraDragging = true;
+					
+					//disable player movement
+					GetComponent<AcrobatPlayer>().maxSpeed = 0;
+					GetComponent<AcrobatPlayer>().jumpForce = 0;
+					GetComponent<StrongPlayer>().maxSpeed = 0;
+					GetComponent<StrongPlayer>().jumpForce = 0;
+					
+					//move mouse to middle of screen
+					Cursor.lockState = CursorLockMode.Locked;
+					Cursor.lockState = CursorLockMode.None;
+					Cursor.visible = true;
+				}
+				else
+				{
+					cameraDragging = false;
+					
+					//enable player movement
+					GetComponent<AcrobatPlayer>().maxSpeed = 6;
+					GetComponent<AcrobatPlayer>().jumpForce = 1;
+					GetComponent<StrongPlayer>().maxSpeed = 6;
+					GetComponent<StrongPlayer>().jumpForce = 1;
+				}
 			}
 
 			/*
@@ -59,7 +89,18 @@ public class PingControls : NetworkBehaviour {
 
 	[Command]
 	void CmdSpawnPing(Vector3 destination){
-		Instantiate(ping, new Vector3(destination.x, destination.y, -1.5f), new Quaternion(90,0,0,0));
-		NetworkServer.Spawn (ping);
+		Instantiate(ping, new Vector3(destination.x, destination.y, -1.5f), new Quaternion(0, 0, 0, 0));
+		//RpcSpawnPing(destination);
+		//NetworkServer.Spawn (ping);
+	}
+
+	[ClientRpc]
+	void RpcSpawnPing(Vector3 destination){
+		Instantiate (ping, new Vector3 (destination.x, destination.y, -1.5f), new Quaternion (0, 0, 0, 0));
+	}
+
+	[ClientCallback]
+	void clientOnlySpawnPing(Vector3 destination){
+		Instantiate (ping, new Vector3 (destination.x, destination.y, -1.5f), new Quaternion (0, 0, 0, 0));
 	}
 }
