0a48c4913a9c6fb148172b16996620dc0539d944
unknown <nas2375@GDD-18.main.ad.rit.edu>
2015-12-06 19:02:44 -0500

    added Ping/Camera Drag GUI Label, added pingOverload to stop ping spam

--- OVWS_Platformer/Assets/Scripts/pingTimer.cs
+++ OVWS_Platformer/Assets/Scripts/pingTimer.cs
@@ -1,17 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.Networking;
 
-public class pingTimer : MonoBehaviour {
+public class pingTimer : NetworkBehaviour {
 
 	private float startTime;
 	private float duration = 4.5f;
+
+	private GameObject[] bothPlayers;
 	void Start () {
 		startTime = Time.time;
+		bothPlayers = GameObject.FindGameObjectsWithTag ("Player");
 	}
 
 	void Update () {
 		if (Time.time > startTime + duration) {
 			Destroy(gameObject);
+			foreach (GameObject player in bothPlayers) {
+				if (player.GetComponent<AcrobatPlayer>().enabled || player.GetComponent<StrongPlayer>().enabled)
+				{
+					player.GetComponent<PingControls>().pingCount--;
+				}
+			}
 		}
 	}
 }
