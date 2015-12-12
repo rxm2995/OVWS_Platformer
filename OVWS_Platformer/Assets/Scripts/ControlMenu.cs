e9c82698b525c04dfb53ffa8e0d67364ee9fdf55
Nicolas San Jose <nas2375@rit.edu>
2015-12-12 17:32:00 -0500

    control menu - only works on host

---
+++ OVWS_Platformer/Assets/Scripts/ControlMenu.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class ControlMenu : NetworkBehaviour {
+
+	bool menuOn;
+	public GameObject menuHolder;
+
+	void Start () {
+		menuOn = false;
+		menuHolder = GameObject.Find ("Menu Holder");
+		menuHolder.SetActive (false);
+	}
+
+	void Update () {
+		if (isLocalPlayer) {
+			if (menuOn && Input.GetKeyDown (KeyCode.P))
+			{
+				Debug.Log ("turning off");
+
+				//disable control menu
+				menuHolder.SetActive(false);
+				menuOn = false;
+
+				/**/ // THIS CAN BE REMOVED, idk if it's appropriate
+				//enable player movement
+				GetComponent<AcrobatPlayer>().maxSpeed = 6;
+				GetComponent<AcrobatPlayer>().jumpForce = 1;
+				GetComponent<StrongPlayer>().maxSpeed = 4;
+				GetComponent<StrongPlayer>().jumpForce = 1.25f;
+				/**/
+			}
+			else if (Input.GetKeyDown (KeyCode.P))
+			{
+				Debug.Log ("turning on");
+
+				//enable control menu
+				menuHolder.SetActive(true);
+				menuOn = true;
+
+				/**/ // THIS CAN BE REMOVED, idk if it's appropriate
+				//disable player movement
+				GetComponent<AcrobatPlayer>().maxSpeed = 0;
+				GetComponent<AcrobatPlayer>().jumpForce = 0;
+				GetComponent<StrongPlayer>().maxSpeed = 0;
+				GetComponent<StrongPlayer>().jumpForce = 0;
+				/**/
+			}
+		}
+	}
+}
